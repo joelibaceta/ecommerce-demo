2b5890f36803120309b7b7909e37d7a6bdd1cdff
Joel Ibaceta <niohnex@gmail.com>
2016-05-02 17:47:48 -0300

    product controller conflicts solved

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,7 @@
 class ProductsController < ApplicationController
   
-  def index
-<<<<<<< HEAD
+  def index 
     category = Category.find(params[:category]) rescue nil
-=======
-    category = Category.find(params[:category])
->>>>>>> f4667346d4ad90a1aa13a01b52d6a8b712e5de31
     @products = Product.all
     @products = @products.where(category_id: category) if category
     @products = @products.order(" created_at DESC ")
