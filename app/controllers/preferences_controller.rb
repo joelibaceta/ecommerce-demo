4956690238e8a3cf7131649d8d5e4c1b958a0ea0
Joel Ibaceta <niohnex@gmail.com>
2016-05-06 16:34:21 -0300

    lib updated

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -1,26 +1,26 @@
-class ItemsController < ApplicationController
+class PreferencesController < ApplicationController
   before_action :authenticate_user!
 
   def new
     cart = Cart.get_current_cart(current_user)
     product = Product.find(params[:product_id])
-    @item = Item.new({product: product, cart: cart})
+    preference = Preference.new({product: product, cart: cart})
   end
 
   def create
-    @item = Item.new(item_params)
-    @item.save
+    preference = Preference.new(item_params)
+    preference.save
     redirect_to products_path
   end
 
   def destroy
-    item = Item.find(params[:id])
+    item = Preference.find(params[:id])
     item.destroy
     redirect_to products_path
   end
 
   private
   def item_params
-    params.require(:item).permit(:quantity, :product_id, :cart_id)
+    params.require(:preference).permit(:quantity, :product_id, :cart_id)
   end
 end
